020a57ea8d41ba7c4d8beb1427cee19c175494f9
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2022-02-18 11:35:37 -0500

    Moved files around

--- coursera/data_structures&algorithms/algorithmic_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ coursera/data_structures&algorithms/algorithmic_toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  int temp = 0;
 
-  // write your code here
+  for (int i = 0; i < values.size(); i++) {
+    for (int j = i; j < values.size(); j++) {
+      if((double)values.at(i)/(double)weights.at(i) < (double)values.at(j)/(double)weights.at(j)) {
+        temp = values.at(i);
+        values.at(i) = values.at(j);
+        values.at(j) = temp;
 
+        temp = weights.at(i);
+        weights.at(i) = weights.at(j);
+        weights.at(j) = temp;
+
+      }
+    }
+
+    for (int i = 0; i < values.size(); i++) {
+      if (capacity == 0) {
+        return value;
+      }
+
+      if (weights.at(i) < capacity) {
+        int min = 0;
+        for (int j = 0; j < i-1; j++) {
+          if (weights.at(j) < weights.at(j+1)) {
+
+          }
+        }
+        capacity -= weights.at(i);
+      }
+    }
+
+  }
+  
+  for (auto x : values) {
+    cout << x << endl;
+   }
+
+  for (auto x : weights) {
+    cout << x << endl;
+  }
   return value;
 }
 
@@ -23,7 +62,7 @@ int main() {
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  cout.precision(10);
+  cout << optimal_value << endl;
   return 0;
 }
