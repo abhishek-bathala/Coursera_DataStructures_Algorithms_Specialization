a11a0fb05cb61a339d14e2d7e39463018c5440ef
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2022-04-20 13:13:11 -0400

    week 4 completed

--- coursera/data_structures&algorithms/algorithmic_toolbox/week4_divide_and_conquer/3_majority_element/majority_element.cpp
+++ coursera/data_structures&algorithms/algorithmic_toolbox/week4_divide_and_conquer/3_majority_element/majority_element.cpp
@@ -6,11 +6,39 @@ using namespace std;
 using std::vector;
 
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
+  //if (left == right) return -1;
+  //if (left + 1 == right) return a[left];
 
-  
-  return -1;
+  int temp = 0;
+  int count_maj_num = 1;
+  int maj_num = a.at(0);
+
+  for (int i = 1; i < a.size(); i++) {
+    if (count_maj_num == 0) {
+      maj_num = a.at(i);
+      count_maj_num = 1;
+    }
+    else {
+      if (maj_num == a.at(i)) {
+        count_maj_num++;
+      }
+      else {
+        count_maj_num--;
+      }
+    }
+  //cout << "num is " << maj_num << " and count is " << count_maj_num << endl;
+  }
+
+  for (int i = 0; i < a.size(); i++) {
+    if (a.at(i) == maj_num) {
+      temp++;
+    }
+  }
+
+  if (temp > a.size()/2) {
+    return 1;
+  }
+  return 0;
 }
 
 int main() {
@@ -20,5 +48,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     cin >> a[i];
   }
-  cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  cout << get_majority_element(a, 0, a.size()-1) << '\n';
 }
