f1227f943c24155c965f5d954b1559db4df0cc05
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2022-07-03 16:45:25 -0400

    week6 problem 1 done

--- coursera/data_structures&algorithms/algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ coursera/data_structures&algorithms/algorithmic_toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+  vector<vector<int>> optimalWeight(w.size()+1, vector<int>(W+1));
+  int value = 0;
+
+  for (int i = 0; i <= w.size(); i++) {
+    optimalWeight[i][0] = 0;
+  }
+  for (int j = 0; j <= W; j++){
+    optimalWeight[0][j] = 0;
+  }
+
+  for (int i = 1; i <= w.size(); i++) {
+    for (int j = 1; j <= W; j++) {
+      optimalWeight[i][j] = optimalWeight[i-1][j];
+
+      if (w[i-1] <= j) {
+        value = optimalWeight[i-1][j-w.at(i-1)] + w[i-1];
+        //cout << "value of " << i << " " << j << " is " << value << " and already is " << optimalWeight[i][j] << endl;
+
+        if (value > optimalWeight[i][j]) {
+          optimalWeight[i][j] = value;
+        }
+      }
     }
   }
-  return current_weight;
+
+  /*cout << endl;
+  for (int i = 0; i <= w.size(); i++) {
+    for (int j = 0; j <= W; j++) {
+      cout << optimalWeight[i][j] << " ";
+    }
+    cout << endl;
+  }*/
+
+  return optimalWeight[w.size()][W];
 }
 
 int main() {
   int n, W;
-  std::cin >> W >> n;
+  cin >> W >> n;
   vector<int> w(n);
   for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
+    cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  cout << optimal_weight(W, w) << '\n';
 }
