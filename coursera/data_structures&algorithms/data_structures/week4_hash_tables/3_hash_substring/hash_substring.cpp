f71bee8b746f1be587f2001768055bcb0ce92d8c
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2022-07-22 20:11:08 -0400

    started problem3 rabin karp algorithm

--- coursera/data_structures&algorithms/data_structures/week4_hash_tables/3_hash_substring/hash_substring.cpp
+++ coursera/data_structures&algorithms/data_structures/week4_hash_tables/3_hash_substring/hash_substring.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <vector>
 
-using std::string;
 typedef unsigned long long ull;
+
+using namespace std;
 
 struct Data {
     string pattern, text;
@@ -11,28 +12,96 @@ struct Data {
 
 Data read_input() {
     Data data;
-    std::cin >> data.pattern >> data.text;
+    cin >> data.pattern >> data.text;
     return data;
 }
 
-void print_occurrences(const std::vector<int>& output) {
-    for (size_t i = 0; i < output.size(); ++i)
-        std::cout << output[i] << " ";
-    std::cout << "\n";
+long long polyHash(string string, long long p, long long x) {
+    long long hash = 0;
+
+    for (int i = string.length()-1; i >= 0; i--) {
+        hash = (hash*x + (int)string[i]) % p;
+    }
+
+    cout << "polyhash is " << hash << endl;
+    return hash;
 }
 
-std::vector<int> get_occurrences(const Data& input) {
-    const string& s = input.pattern, t = input.text;
-    std::vector<int> ans;
+vector<long long> precomputeHashes(string text, int patternLength, long long p, long long x) {
+    cout << "precomputing" << endl;
+    vector<long long> hashes(text.length()-patternLength+1);
+    string substring = text.substr(text.length()-patternLength, text.length()-1);
+    hashes[text.length()-patternLength] = polyHash(text, p, x);
+    int y = 1;
+
+    for (int i = 1; i < patternLength; i++) {
+        y = (y*x) % p;
+    }
+
+    for (int i = text.length()-patternLength-1; i >= 0; i--) {
+        hashes[i] = (x*hashes[i+1] + (int)text[i] - y*text[i + patternLength]) % p;
+    }
+
+    return hashes;
+}
+
+bool areEqual (string s1, string s2) {
+    cout << "areequal" << endl;
+    if (s1.length() != s2.length()) {
+        return false;
+    }
+
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+vector<int> rabinKarp(string text, string pattern)  {
+    long long p = 99999999977;
+    long long x = 1;
+    vector<int> positions;
+    long long pHash = polyHash(pattern, p, x);
+    vector<long long> hashes = precomputeHashes(text, pattern.length(), p, x);
+
+    for (int i = 0; i < text.length()-pattern.length(); i++) {
+        cout << "i is " << i << endl;
+        if (pHash != hashes[i]) {
+            continue;
+        }
+        if (areEqual(text.substr(i+pattern.length()-1), pattern)) {
+            cout << "pushingback" << endl;
+            positions.push_back(i);
+        }
+    }
+
+    return positions;
+
+}
+
+void print_occurrences(const vector<int>& output) {
+    for (size_t i = 0; i < output.size(); ++i)
+        cout << output[i] << " ";
+    cout << "\n";
+}
+
+vector<int> get_occurrences(const Data& input) {
+    /*const string& s = input.pattern, t = input.text;
+    vector<int> ans;
     for (size_t i = 0; i + s.size() <= t.size(); ++i)
         if (t.substr(i, s.size()) == s)
             ans.push_back(i);
-    return ans;
+    return ans;*/
+
+    return rabinKarp(input.text, input.pattern);
 }
 
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     print_occurrences(get_occurrences(read_input()));
     return 0;
 }
