a2a2220949e69a68f98d1228c398cb06c92c049b
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2022-07-25 17:26:34 -0400

    week4 done

--- coursera/data_structures&algorithms/data_structures/week4_hash_tables/2_hash_chains/hash_chains.cpp
+++ coursera/data_structures&algorithms/data_structures/week4_hash_tables/2_hash_chains/hash_chains.cpp
@@ -3,9 +3,7 @@
 #include <vector>
 #include <algorithm>
 
-using std::string;
-using std::vector;
-using std::cin;
+using namespace std;
 
 struct Query {
     string type, s;
@@ -14,9 +12,8 @@ struct Query {
 
 class QueryProcessor {
     int bucket_count;
-    // store all strings in one vector
-    vector<string> elems;
     size_t hash_func(const string& s) const {
+        //cout << "hash function" << endl;
         static const size_t multiplier = 263;
         static const size_t prime = 1000000007;
         unsigned long long hash = 0;
@@ -42,32 +39,63 @@ public:
         std::cout << (was_found ? "yes\n" : "no\n");
     }
 
-    void processQuery(const Query& query) {
-        if (query.type == "check") {
-            // use reverse order, because we append strings to the end
-            for (int i = static_cast<int>(elems.size()) - 1; i >= 0; --i)
-                if (hash_func(elems[i]) == query.ind)
-                    std::cout << elems[i] << " ";
-            std::cout << "\n";
-        } else {
-            vector<string>::iterator it = std::find(elems.begin(), elems.end(), query.s);
-            if (query.type == "find")
-                writeSearchResult(it != elems.end());
-            else if (query.type == "add") {
-                if (it == elems.end())
-                    elems.push_back(query.s);
-            } else if (query.type == "del") {
-                if (it != elems.end())
-                    elems.erase(it);
-            }
-        }
-    }
-
     void processQueries() {
+        //cout << "processing" << endl;
+        vector<vector<string>> elems(bucket_count);
+        //cout << "size of elems is " << elems.size() << endl;
         int n;
         cin >> n;
-        for (int i = 0; i < n; ++i)
-            processQuery(readQuery());
+        for (int i = 0; i < n; ++i) {
+            Query query = readQuery();
+            if (query.type == "check") {
+            // use reverse order, because we append strings to the end
+                for (int i = elems[query.ind].size()-1; i >= 0; i--) {
+                    cout << elems[query.ind].at(i) << " ";
+                }
+                cout << endl;
+            } else {
+                size_t hashValue = hash_func(query.s);
+                if (query.type == "find") {
+                    //cout << "finding" << endl;
+                    bool found = false;
+
+                    for (int i = 0; i < elems[hashValue].size(); i++) {
+                        if (elems[hashValue].at(i) == query.s) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    writeSearchResult(found);
+                }
+                else if (query.type == "add") {
+                    //cout << "adding" << endl;
+                    bool found = false;
+                    for (int i = 0; i < elems[hashValue].size(); i++) {
+                        if (elems[hashValue].at(i) == query.s) {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        elems[hashValue].push_back(query.s);
+                        //cout << "added " << query.s << " to " << hashValue << " size is now " << elems[hashValue].size() << endl;
+                    }
+
+                } else if (query.type == "del") {
+                    //cout << "deleting hash " << hashValue << " with size " << elems[hashValue].size() << endl;
+                    for (int i = 0; i < elems[hashValue].size(); i++) {
+                        //cout << "comparing " << query.s << " with " << elems[hashValue].at(i);
+                        if (elems[hashValue].at(i) == query.s) {
+                            //cout << "deleted" << query.s << " from " << hashValue << endl;
+                            elems[hashValue].erase(elems[hashValue].begin() + i);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
     }
 };
 
