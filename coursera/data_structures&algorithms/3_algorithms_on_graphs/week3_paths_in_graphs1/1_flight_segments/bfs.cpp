972a4c0d4ff82590dbe7a20017c72ffc1af9a299
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2022-08-24 15:15:48 -0400

    week3 completed (bfs)

--- coursera/data_structures&algorithms/3_algorithms_on_graphs/week3_paths_in_graphs1/1_flight_segments/bfs.cpp
+++ coursera/data_structures&algorithms/3_algorithms_on_graphs/week3_paths_in_graphs1/1_flight_segments/bfs.cpp
@@ -2,26 +2,47 @@
 #include <vector>
 #include <queue>
 
-using std::vector;
-using std::queue;
+using namespace std;
 
 int distance(vector<vector<int> > &adj, int s, int t) {
   //write your code here
-  return -1;
+  vector<int> distances (adj.size(), INT8_MAX);
+  vector<int> queue;
+  int index = -1;
+
+  distances[s] = 0;
+  queue.push_back(s);
+
+  //BFS
+  while (!queue.empty()) {
+    index = queue[0];
+    queue.erase(queue.begin());
+    for (int i = 0; i < adj[index].size(); i++) {
+      if (distances[adj[index].at(i)] == INT8_MAX) {
+        queue.push_back(adj[index].at(i));
+        distances[adj[index].at(i)] = distances[index]+1;
+      }
+    }
+  }
+
+  if (distances[t] == INT8_MAX) {
+    return -1;
+  }
+  return distances[t];
 }
 
 int main() {
   int n, m;
-  std::cin >> n >> m;
+  cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   for (int i = 0; i < m; i++) {
     int x, y;
-    std::cin >> x >> y;
+    cin >> x >> y;
     adj[x - 1].push_back(y - 1);
     adj[y - 1].push_back(x - 1);
   }
   int s, t;
-  std::cin >> s >> t;
+  cin >> s >> t;
   s--, t--;
-  std::cout << distance(adj, s, t);
+  cout << distance(adj, s, t);
 }
