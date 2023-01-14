f22c4b203b983c4adc751c89cfade181cf630535
abhishek-bathala <abhishek.bathala@outlook.com>
2023-01-13 22:17:31 -0500

    week1 problem 1 completed

--- coursera/data_structures&algorithms/4_algorithms_on_graphs/week1_suffix_trees/1_trie/trie.cpp
+++ coursera/data_structures&algorithms/4_algorithms_on_graphs/week1_suffix_trees/1_trie/trie.cpp
@@ -8,6 +8,7 @@ using namespace std;
 struct node {
   char symbol = ' ';
   int index = -1;
+  int parent = -1;
   vector<int> branches;
 };
 
@@ -22,33 +23,56 @@ trie build_trie(vector<string> & patterns) {
   node root;
   root.index = 0;
   t.push_back(root);
+  bool branch_found = false;
+  int branch_index = -1;
   // write your code here
   for (int i = 0; i < patterns.size(); i++) {
     current_node = 0;
     pattern = patterns[i]; 
     for (int j = 0; j < pattern.size(); j++) {
       current_symbol = pattern[j];
-      cout << "current symbol is " << current_symbol << endl;
+      //cout << "current symbol is " << current_symbol << endl;
       if (t[current_node].branches.size() == 0) {
         node newNode;
         size++;
         newNode.index = size;
         newNode.symbol = current_symbol;
+        newNode.parent = current_node;
+        //cout << "parent of " << newNode.index << " is " << newNode.parent << endl;
+        //cout << "added " << newNode.symbol << " at index " << newNode.index << endl;
         t.push_back(newNode);
         t[current_node].branches.push_back(newNode.index);
-        cout << "added " << newNode.symbol << " at index " << newNode.index << endl;
         current_node = newNode.index;
-        cout << "\tcurrentnode is now " << current_node << endl;
+        //cout << "\tcurrentnode is now " << current_node << endl;
       }
       else {
         for (int k = 0; k < t[current_node].branches.size(); k++) {
           if (t[t[current_node].branches[k]].symbol == current_symbol) {
-            current_node = t[current_node].branches[k];
-            cout << "\t\tcurrentnode is now " << current_node << endl;
+            branch_found = true;
+            branch_index = k;
           }
         }
+        if (branch_found) {
+          current_node = t[current_node].branches[branch_index];
+          //cout << "\t\tmoving down the trie, currentnode is now " << current_node << endl;
+        }
+        else {
+          node newNode;
+          size++;
+          newNode.index = size;
+          newNode.symbol = current_symbol;
+          newNode.parent = current_node;
+          //cout << "parent of " << newNode.index << " is " << newNode.parent << endl;
+          //cout << "added " << newNode.symbol << " at index " << newNode.index << endl;
+          t.push_back(newNode);
+          t[current_node].branches.push_back(newNode.index);
+          current_node = newNode.index;
+          //cout << "\tcurrentnode is now " << current_node << endl;
+        }
       }
-    }        
+      branch_found = false;
+    }
+    //cout << "\t\tgoing to next word" << endl;
   }
   return t;
 }
@@ -64,12 +88,10 @@ int main() {
   }
 
   trie t = build_trie(patterns);
-  cout << endl << endl;
+  //cout << endl << endl;
   for (size_t i = 0; i < t.size(); ++i) {
-    cout << "i is " << i << " whose symbol is " << t[i].symbol << endl;
-    cout << "i has " << t[i].branches.size() << " branches" << endl;
-    for (size_t j = 0; j < t[i].branches.size(); j++) {
-      cout << i << "->" << j << endl;
+    if (t[i].index != 0) {
+      cout << t[i].parent << "->" << t[i].index << ":" << t[i].symbol << endl;
     }
   }
 
