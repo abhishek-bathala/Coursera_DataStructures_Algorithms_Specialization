ae735bc3f1bf74f325c947562fdab130cda6bbf9
abhishek-bathala <abhishek.bathala@outlook.com>
2023-02-21 13:57:36 -0500

    course4 week2 completed

--- coursera/data_structures&algorithms/4_algorithms_on_graphs/week2_bwt&suffix_arrays/2_bwtinverse/bwtinverse.cpp
+++ coursera/data_structures&algorithms/4_algorithms_on_graphs/week2_bwt&suffix_arrays/2_bwtinverse/bwtinverse.cpp
@@ -3,17 +3,44 @@
 #include <string>
 #include <vector>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
+using namespace std;
+
+struct FirstColumn
+{
+  char letter;
+  int index;
+
+  FirstColumn(char c, int i) {
+    letter = c;
+    index = i;
+  }
+
+  bool operator< (const FirstColumn &x) {
+    if (letter == x.letter) {
+      return index < x.index;
+    }
+    return letter < x.letter;
+  }
+};
 
 string InverseBWT(const string& bwt) {
   string text = "";
+  string lastColumn = bwt;
 
-  // write your code here
+  vector<FirstColumn> firstColumn;
+  for (int i = 0; i < bwt.length(); i++) {
+    firstColumn.push_back(FirstColumn(bwt[i], i));
+  }
 
+  sort(firstColumn.begin(), firstColumn.end());
+
+  int index = 0;
+  for (int i = 0; i < firstColumn.size(); i++) {
+    //cout << firstColumn[i].letter << firstColumn[i].index << endl;
+    index = firstColumn[index].index;
+    text += firstColumn[index].letter;
+    //cout << text << endl;
+  }
   return text;
 }
 
